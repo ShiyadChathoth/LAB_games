5e36a53027023a445e00e405406fe9d383d56f67
ShiyadChathoth <shiyadchathoth@gmail.com>
2026-07-10 19:19:03 +0530

    Remove unwanted tracked files

--- android/app/src/main/kotlin/com/example/booking_monitor_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.booking_monitor_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- server_monitor-main/android/app/src/main/kotlin/com/example/pi_monitor_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.pi_monitor_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
